73ce7238a8e6975dc08c2549ceb8ce40f19268a0
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2024-08-23 11:05:43 -0600

    feat: base front-end structure

---
+++ apps/frontend/src/components/icons/index.tsx
@@ -0,0 +1,17 @@
+import { LinkedinIcon } from "lucide-react";
+import {
+  BookMarkIcon,
+  GithubIcon,
+  HomeIcon,
+  ProjectIcon,
+  WritingIcon,
+} from "./shared";
+
+export const Icons = {
+  home: HomeIcon,
+  writing: WritingIcon,
+  bookmark: BookMarkIcon,
+  project: ProjectIcon,
+  github: GithubIcon,
+  linkedIn: LinkedinIcon,
+};
